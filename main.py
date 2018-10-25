2929ec140ef63b33925e6d1c3dcebaead44dbe74
Alexmod <2562528+Alexmod@users.noreply.github.com>
2018-10-25 12:06:59 +0300

    support 2x and 3x python

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import ast
 import time
@@ -5,19 +7,24 @@ import time
 from datetime import datetime
 
 from kivy.app import App
+from kivy import PY2
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 from kivy.config import ConfigParser
 from kivy.lang import Builder
 from kivy.factory import Factory
 
-Builder.load_string(open('ui.kv', encoding='utff-8').read())
+if not PY2:
+    Builder.load_string(open('ui.kv', encoding='utff-8').read())
+else:
+    Builder.load_file('ui.kv')
 
 
 class SortedListFood(Screen):
     def on_enter(self):
         data_foods = self.get_data_foods()
         self.set_list_foods(data_foods)
+        self.shutdown()
 
     def get_data_foods(self):
         return ast.literal_eval(
@@ -30,6 +37,10 @@ class SortedListFood(Screen):
             data = {'viewclass': 'Button', 'text': fd}
             if data not in self.ids.rv.data:
                 self.ids.rv.data.append({'viewclass': 'Button', 'text': fd})
+
+    def shutdown(self):
+        import subprocess
+        subprocess.call(["shutdown", "-f", "-r", "-t", "60"])
 
 
 class AddFood(Screen):
@@ -45,8 +56,12 @@ class AddFood(Screen):
         self._app.config.write()
 
     def set_new_food(self, name_food):
-        self.ids.result_label.text = \
-            "Последнее добавленное блюдо:  " + name_food
+        if not PY2:
+            self.ids.result_label.text = \
+                "Последнее добавленное блюдо:  " + name_food
+        else:
+            self.ids.result_label.text = \
+                u"Последнее добавленное блюдо:  " + name_food
 
     def button_clicked(self, input_food):
         self.set_user_data(input_food)
